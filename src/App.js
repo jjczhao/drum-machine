cf376f819b94d9821a641c8e7d6a7c91898ee499
jjczhao <jasonzhao15@gmail.com>
2019-05-09 01:08:20 -0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Drum from './Drum';
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <Drum />
+    </div>
+  );
+}
+
+export default App;
